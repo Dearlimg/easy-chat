466bc4360e2d890110de02928cd78dcb3115c1a7
“username” <email>
2025-05-11 21:04:09 +0800

    Init

---
+++ apps/user/exec.sh
@@ -0,0 +1,4 @@
+goctl rpc protoc ./apps/user/rpc/user.proto --go_out=./apps/user/rpc/ --go-grpc_out=./apps/user/rpc/ --zrpc_out=./apps/user/rpc/
+
+
+ goctl rpc protoc user.proto --go_out=./ --go-grpc_out=./ --zrpc_out=./
---
+++ apps/user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "chat-mic/apps/user/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
